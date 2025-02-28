857b3266bb705678f37359ffd509749b3ae93b7e
slim-rie <pontino.johnjerriehl.00246@dyci.edu.ph>
2025-02-28 14:26:49 +0800

    Add files via upload

---
+++ sadDb.sql
@@ -0,0 +1,10 @@
+ create database sadDb;
+ 
+ use sadDb;
+ 
+ select * from User;
+ 
+INSERT INTO User (email, first_name, middle_name, last_name, password, role)
+VALUES ('johnjerriehlp@gmail.com', 'John Jerriehl', '', 'Pontino', '12345678', 'admin');
+
+
