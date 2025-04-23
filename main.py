2f84426b954e3df8f8de3624525eb3285576594b
Ariane Sudaria <arianesudaria@gmail.com>
2025-04-23 20:53:32 +0800

    staff admin (x functions)

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24).hex()  # Generate a secure random key
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
